ef7e70d123e2b6294ae8606e827c1de517359a76
igorskyflyer <igor.dvlpr@gmail.com>
2022-09-08 00:05:28 +0200

    Markdown tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,17 @@
 import { defineConfig } from 'astro/config'
+import { readingTime } from './src/js/reading-time.mjs'
+readingTime
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://igorskyflyer.github.io',
+  markdown: {
+    extendDefaultPlugins: true,
+    remarkPlugins: [readingTime],
+    shikiConfig: {
+      theme: 'one-dark-pro',
+      langs: [],
+      wrap: true,
+    },
+  },
 })
