d6c6a563ce72480fc6f922e921a432b72de0b2fc
Igor Dimitrijević <igor.dvlpr@gmail.com>
2022-09-13 03:43:20 +0200

    Add postDate function

--- src/functions/posts.ts
+++ src/functions/posts.ts
@@ -3,3 +3,9 @@ import type { MarkdownInstance } from 'astro'
 export function sortPosts(posts: MarkdownInstance<Record<string, any>>[]) {
   return posts.sort((a, b) => new Date(b.frontmatter.publishDate).valueOf() - new Date(a.frontmatter.publishDate).valueOf())
 }
+
+export function postDate(date: string) {
+ const postDate = Date.parse(date)
+ const days = [ 'SUN', 'MON', 'TUE', 'WED', 'THU', 'SAT' ]
+ return `${days[postDate.getDay()}`
+}
