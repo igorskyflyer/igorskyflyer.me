93d38a81506b29502e0fa4b905ab360f7ee6e0a1
Igor Dimitrijević <igor.dvlpr@gmail.com>
2022-09-13 06:14:06 +0200

    Update date format

--- src/functions/posts.ts
+++ src/functions/posts.ts
@@ -7,5 +7,6 @@ export function sortPosts(posts: MarkdownInstance<Record<string, any>>[]) {
 export function postDate(date: string) {
  const postDate = new Date(Date.parse(date))
  const days = [ 'SUN', 'MON', 'TUE', 'WED', 'THU', 'SAT' ]
- return `${days[postDate.getDay()]} ${date}`
+ const months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
+ return `${days[postDate.getDay()]} ${postDate.getDate()} ${months[postDate.getMonth()] ${postDate.getFullYear()}`
 }
