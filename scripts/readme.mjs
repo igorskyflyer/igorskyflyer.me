b1449258517727615501fddc93234569fdd5e15d
igorskyflyer <igor.dvlpr@gmail.com>
2022-09-14 23:29:02 +0200

    Add README automation script

---
+++ scripts/readme.mjs
@@ -0,0 +1,8 @@
+import { writeFile } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+
+const readmeUrl = 'https://raw.githubusercontent.com/igorskyflyer/igorskyflyer/main/README.md'
+const response = await fetch(readmeUrl)
+const markdown = await response.text()
+const outFile = fileURLToPath(new URL('../src/pages/igorskyflyer.md', import.meta.url))
+await writeFile(outFile, markdown)
