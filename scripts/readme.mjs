2171276e14d0648f1ef84665bda95078b3366003
igorskyflyer <igor.dvlpr@gmail.com>
2022-09-15 00:21:13 +0200

    Inject frontmatter to README

--- scripts/readme.mjs
+++ scripts/readme.mjs
@@ -4,5 +4,14 @@ import { fileURLToPath } from 'node:url'
 const readmeUrl = 'https://raw.githubusercontent.com/igorskyflyer/igorskyflyer/main/README.md'
 const response = await fetch(readmeUrl)
 const markdown = await response.text()
+const frontmatter = `---
+title: IgorSkyFlyer
+layout: ../layouts/post.astro
+publishDate: 2022-09-15 00:00
+---
+`
+const fileContents = `${frontmatter}
+${markdown}`
+
 const outFile = fileURLToPath(new URL('../src/pages/igorskyflyer.md', import.meta.url))
-await writeFile(outFile, markdown)
+await writeFile(outFile, fileContents)
