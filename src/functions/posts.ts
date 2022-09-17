3f46d15f0e4542439758cfe8bbb2ea1e6cb7020c
igorskyflyer <igor.dvlpr@gmail.com>
2022-09-17 05:33:29 +0200

    Update postDate function

--- src/functions/posts.ts
+++ src/functions/posts.ts
@@ -5,14 +5,33 @@ export function sortPosts(posts: MarkdownInstance<Record<string, any>>[]) {
 }
 
 export function postDate(date: string) {
- const postDate = new Date(Date.parse(date))
- const days = [ 'SUN', 'MON', 'TUE', 'WED', 'THU', 'SAT' ]
- const months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
- let hours = postDate.getHours()
- let minutes = postDate.getMinutes()
+  const postDate = new Date(Date.parse(date))
+  const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+  let hours = postDate.getHours()
+  let minutes = postDate.getMinutes()
 
- if(hours < 10) { hours = `0${hours}` }
- if(minutes < 10) { minutes = `0${minutes}` }
+  if (hours < 10) {
+    hours = `0${hours}`
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
 
- return `${days[postDate.getDay()]}, ${postDate.getDate()} ${months[postDate.getMonth()]} ${postDate.getFullYear()} @ ${hours}:${minutes}h`
+  return `${days[postDate.getDay()]}, ${postDate.getDate()} ${
+    months[postDate.getMonth()]
+  } ${postDate.getFullYear()} @ ${hours}:${minutes}h`
 }
