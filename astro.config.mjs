3da7c2ae44ea88a535cc4865ff995b6ab7d41f6c
igorskyflyer <igor.dvlpr@gmail.com>
2023-09-26 01:21:09 +0200

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,47 +1,50 @@
-import { defineConfig } from 'astro/config';
-import compress from 'astro-compress';
-import { readingTime } from './src/functions/reading-time';
-import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
+import { defineConfig } from 'astro/config'
+import compress from 'astro-compress'
+import { readingTime } from './src/functions/reading-time'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 export default defineConfig({
-  site: 'https://igorskyflyer.github.io',
+  site: 'https://igorskyflyer.me',
   vite: {
     build: {
       rollupOptions: {
         output: {
-          entryFileNames: 'entry.[hash].js',
+          entryFileNames: 'script.[hash].js',
           chunkFileNames: 'chunks/chunk.[hash].js',
-          assetFileNames: 'css/style.[hash][extname]'
-        }
-      }
+          assetFileNames: 'css/ui.[hash][extname]',
+        },
+      },
     },
     ssr: {
-      noExternal: ['astro', '@igor.dvlpr/astro-post-excerpt']
-    }
+      noExternal: ['astro', '@igor.dvlpr/astro-post-excerpt'],
+    },
   },
   markdown: {
     remarkPlugins: [readingTime],
     shikiConfig: {
       theme: 'one-dark-pro',
-      wrap: true
-    }
-  },
-  integrations: [compress({
-    html: {
-      collapseWhitespace: true,
-      minifyJS: true,
-      minifyCSS: true,
-      quoteCharacter: '"',
-      html5: true,
-      removeAttributeQuotes: false,
-      removeTagWhitespace: false,
-      keepClosingSlash: false,
-      removeStyleLinkTypeAttributes: false,
-      removeScriptTypeAttributes: false
+      wrap: true,
     },
-    css: {
-      restructure: true
-    }
-  }), mdx()]
-});
+  },
+  integrations: [
+    compress({
+      html: {
+        collapseWhitespace: true,
+        minifyJS: true,
+        minifyCSS: true,
+        quoteCharacter: '"',
+        html5: true,
+        removeAttributeQuotes: false,
+        removeTagWhitespace: false,
+        keepClosingSlash: false,
+        removeStyleLinkTypeAttributes: false,
+        removeScriptTypeAttributes: false,
+      },
+      css: {
+        restructure: true,
+      },
+    }),
+    mdx(),
+    sitemap(),
+  ],
+})
