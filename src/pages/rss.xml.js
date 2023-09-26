504dd90cf4409fa056ce449614cdf11bc377f7ff
igorskyflyer <igor.dvlpr@gmail.com>
2023-09-26 04:18:58 +0200

    Add RSS feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,25 @@
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+
+export async function GET(context) {
+  const blog = (await getCollection('blog')).filter((post) => {
+    if (!post.data.draft) {
+      return post
+    }
+  })
+
+  return rss({
+    title: 'IgorSkyFlyer.me',
+    description: 'A place where I like to express myself.',
+    site: context.site,
+    items: blog.map((post) => {
+      return {
+        title: post.data.title,
+        pubDate: post.data.publishDate,
+        description: post.data.description,
+        customData: `<language>en-us</language>`,
+        link: `/blog/${post.slug}/`,
+      }
+    }),
+  })
+}
