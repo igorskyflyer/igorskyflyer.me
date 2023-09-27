7ae0d561a2f3127e79fc5840fbb2385e37e03136
igorskyflyer <igor.dvlpr@gmail.com>
2023-09-27 17:30:31 +0200

    Use tags array directly

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -29,12 +29,10 @@ export async function GET(context) {
       )}" rel="self" type="application/rss+xml" />`,
     ].join(''),
     items: blog.map((post) => {
-      const tags = post.data.tags.split(',')
-
       return {
         title: post.data.title,
         link: `/blog/${post.slug}/`,
-        categories: tags,
+        categories: post.data.tags,
         pubDate: post.data.publishDate,
         description: post.data.description,
         customData: '<dc:creator>Igor Dimitrijević (igorskyflyer)</dc:creator>',
