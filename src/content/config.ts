9cc87549db283b8d4a3ea3d51132cb8e57754d8b
igorskyflyer <igor.dvlpr@gmail.com>
2023-09-27 17:08:51 +0200

    Update publishDate field

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,7 @@ const blogCollection = defineCollection({
     title: z.string(),
     description: z.string(),
     featuredImage: z.string().optional(),
-    publishDate: z.string(),
+    publishDate: z.string().transform((str) => new Date(str)),
     tags: z.string(),
     draft: z.boolean().default(false),
   }),
