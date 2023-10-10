e1d8c6151eb32c4d2b94d4eb652a83c82e6c5aa1
igorskyflyer <igor.dvlpr@gmail.com>
2023-10-10 16:23:08 +0200

    Update README caching

--- scripts/readme.mjs
+++ scripts/readme.mjs
@@ -1,5 +1,10 @@
+import { existsSync, mkdirSync } from 'node:fs'
 import { writeFile } from 'node:fs/promises'
 import { fileURLToPath } from 'node:url'
+
+if (!existsSync('_remote')) {
+  mkdirSync('_remote')
+}
 
 const readmeUrl =
   'https://raw.githubusercontent.com/igorskyflyer/igorskyflyer/main/README.md'
@@ -7,6 +12,6 @@ const response = await fetch(readmeUrl)
 const markdown = await response.text()
 
 const outFile = fileURLToPath(
-  new URL('../.remote/igorskyflyer.md', import.meta.url)
+  new URL('../_remote/igorskyflyer.md', import.meta.url)
 )
 await writeFile(outFile, markdown)
