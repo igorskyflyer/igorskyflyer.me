4c93e53b494037c1af24ce7c3a4d15ec94bbb552
igorskyflyer <igor.dvlpr@gmail.com>
2023-10-10 14:59:47 +0200

    Use custom page instead of layout

--- scripts/readme.mjs
+++ scripts/readme.mjs
@@ -1,15 +1,12 @@
 import { writeFile } from 'node:fs/promises'
 import { fileURLToPath } from 'node:url'
 
-const readmeUrl = 'https://raw.githubusercontent.com/igorskyflyer/igorskyflyer/main/README.md'
+const readmeUrl =
+  'https://raw.githubusercontent.com/igorskyflyer/igorskyflyer/main/README.md'
 const response = await fetch(readmeUrl)
 const markdown = await response.text()
-const frontmatter = `---
-layout: ../layouts/author.astro
----
-`
-const fileContents = `${frontmatter}
-${markdown}`
 
-const outFile = fileURLToPath(new URL('../src/pages/igorskyflyer.md', import.meta.url))
-await writeFile(outFile, fileContents)
+const outFile = fileURLToPath(
+  new URL('../src/pages/igorskyflyer.md', import.meta.url)
+)
+await writeFile(outFile, markdown)
