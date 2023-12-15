5df1c7b0f738b15032b010227bb9c2dc58157bf4
igorskyflyer <igor.dvlpr@gmail.com>
2023-12-15 21:27:52 +0100

    💄 Reformat

--- scripts/readme.mjs
+++ scripts/readme.mjs
@@ -3,15 +3,15 @@ import { writeFile } from 'node:fs/promises'
 import { fileURLToPath } from 'node:url'
 
 if (!existsSync('_remote')) {
-  mkdirSync('_remote')
+	mkdirSync('_remote')
 }
 
 const readmeUrl =
-  'https://raw.githubusercontent.com/igorskyflyer/igorskyflyer/main/README.md'
+	'https://raw.githubusercontent.com/igorskyflyer/igorskyflyer/main/README.md'
 const response = await fetch(readmeUrl)
 const markdown = await response.text()
 
 const outFile = fileURLToPath(
-  new URL('../_remote/igorskyflyer.md', import.meta.url)
+	new URL('../_remote/igorskyflyer.md', import.meta.url)
 )
 await writeFile(outFile, markdown)
