b257fd3b0ec44bd93a13fdfb188d305594387f04
igorskyflyer <igor.dvlpr@gmail.com>
2024-07-11 00:02:30 +0200

    Remove description property for posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,14 +3,13 @@ import { defineCollection, z } from 'astro:content'
 const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
-    description: z.string(),
     featuredImage: z.string().optional(),
     publishDate: z.string().transform((str) => new Date(str)),
     tags: z.array(z.string()),
-    draft: z.boolean().default(false),
-  }),
+    draft: z.boolean().default(false)
+  })
 })
 
 export const collections = {
-  blog: blogCollection,
+  blog: blogCollection
 }
