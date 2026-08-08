ff4eaa386d6dddfc035c6e2b8304afe225b73513
igorskyflyer <igor.dvlpr@gmail.com>
2026-08-08 02:58:43 +0200

    🔨 logic(footer): add update date

--- src/functions/utilities.ts
+++ src/functions/utilities.ts
@@ -1,3 +1,16 @@
 export function getYear() {
   return new Date().getFullYear()
 }
+
+export function getLocalDate() {
+  return new Date().toLocaleString('en-uk', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hourCycle: 'h23',
+    timeZone: 'Europe/Belgrade',
+    timeZoneName: 'longOffset'
+  })
+}
