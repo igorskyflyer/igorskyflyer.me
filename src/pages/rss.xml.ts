94fd4630dbe5f160863d114adf0fe46d32eb7a6e
igorskyflyer <igor.dvlpr@gmail.com>
2026-08-09 18:59:59 +0200

    🔨 logic(rss): update RSS feed to use TypeScript

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,45 @@
+import rss from '@astrojs/rss'
+import { SiteName } from '@data/Site.ts'
+import type { APIContext } from 'astro'
+import { getCollection } from 'astro:content'
+
+export async function GET(context: APIContext) {
+  const siteUrl = context.site ?? new URL('https://igorskyflyer.me')
+
+  const posts = (await getCollection('blog'))
+    .filter((post) => !post.data.draft)
+    .sort(
+      (a, b) =>
+        new Date(b.data.publishDate).getTime() -
+        new Date(a.data.publishDate).getTime()
+    )
+
+  return rss({
+    title: `Software Engineering & Developer Experience Blog | ${SiteName}`,
+    description:
+      'Technical insights on TypeScript, AST compilers, custom DSLs, open-source tooling, and modern full-stack development by Igor Dimitrijević (igorskyflyer).',
+    site: siteUrl,
+    stylesheet: '/rss-styles.xsl',
+    xmlns: {
+      atom: 'http://www.w3.org/2005/Atom',
+      dc: 'http://purl.org/dc/elements/1.1/'
+    },
+    customData: [
+      '<language>en-us</language>',
+      `<atom:link href="${new URL('rss.xml', siteUrl)}" rel="self" type="application/rss+xml" />`
+    ].join(''),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      link: `/blog/${post.id}/`,
+      categories: post.data.tags ?? [],
+      pubDate: new Date(post.data.publishDate),
+      // description: post.data.description,
+      customData: [
+        '<dc:creator>Igor Dimitrijević (igorskyflyer)</dc:creator>',
+        post.data.featuredImage
+          ? `<enclosure url="${new URL(post.data.featuredImage, siteUrl)}" type="image/png" length="0" />`
+          : ''
+      ].join('')
+    }))
+  })
+}
