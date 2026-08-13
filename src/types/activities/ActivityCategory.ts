ef2953aff07552daa6f828651f450e6058445c9a
igorskyflyer <igor.dvlpr@gmail.com>
2026-08-13 22:06:27 +0200

    📐 types: add ActivityCategory type

---
+++ src/types/activities/ActivityCategory.ts
@@ -0,0 +1,13 @@
+// Author: Igor Dimitrijević (@igorskyflyer)
+
+export type ActivityCategory =
+  | 'certification'
+  | 'contribution'
+  | 'identity'
+  | 'milestone'
+  | 'package'
+  | 'publication'
+  | 'release'
+  | 'repository'
+  | 'education'
+  | 'award'
