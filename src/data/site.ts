c525a62cef299f1f0e683ae5ae523530fd2dc6a2
igorskyflyer <igor.dvlpr@gmail.com>
2026-08-14 18:33:41 +0200

    🔩 config(site): update default page title

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,4 +1,4 @@
 // Author: Igor Dimitrijević (@igorskyflyer)
 
-export const SiteName = 'Igor Dimitrijević'
+export const SiteName = 'Igor Dimitrijević (igorskyflyer)'
 export const PostsPerPage = 5
